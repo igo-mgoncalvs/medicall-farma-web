08a496252401498f008a5e25e261270b7a7a6150
Igor Goncalves <igor2013mgoncalves@gmail.com>
2024-04-08 17:47:26 -0300

    integração da pagina de produtos e detalhes

---
+++ src/app/produtos/[route]/page.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+
+import goBack from '@/assets/icons/goBack.svg'
+
+import styles from './styles.module.css'
+import BASE_URL from '@/hooks/axios'
+import Link from 'next/link'
+
+interface IProduct {
+  id: string,
+  image: string
+  name: string
+  link: string
+  description: string
+}
+
+async function getData({ route } : { route: string }) {
+  return BASE_URL.get<IProduct>(`/find-product/${route}`)
+    .then((data) => data.data)
+}
+
+async function ProductsDetails ({ params }: { params: { route: string } }) {
+  const data = await getData({ route: params.route })
+
+  return (
+    <div className={styles.products_details}>
+      <Link
+        className={styles.product_goBack_container}
+        href={'/produtos'}
+      >
+        <Image
+          src={goBack}
+          className={styles.product_goBack_icon}
+          alt='icone de voltar'
+        />
+
+        <p>Voltar</p>
+      </Link>
+
+      <div className={styles.products_details_container}>
+        <img
+          alt={`imagem do produto ${data.name}`}
+          src={data.image}
+          className={styles.product_image}
+        />
+        <div>
+          <div className={styles.text_container}>
+            <p className={styles.title}>
+              {data.name}
+            </p>
+            <p className={styles.text}>
+              {data.description}
+            </p>
+          </div>
+
+          <div className={styles.buttons_container}>
+            <a
+              className={styles.contac_button}
+              href={data.link}
+              target='_blank'
+              >
+              Entrar em contato
+            </a>
+            <a className={styles.catalog_button}
+              target='_blank'
+              href={data.link}
+            >
+              Baixar catálogo
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductsDetails
---
+++ src/components/pageProductsClient/page.tsx
@@ -0,0 +1,179 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import React, { useState, useEffect, useCallback } from 'react'
+import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import search_logo from '@/assets/icons/search.svg'
+import whatsapp from '@/assets/icons/whatsapp.svg'
+
+import styles from './styles.module.css'
+import 'swiper/css';
+
+interface IProduct {
+  id: string
+  image: string 
+  name: string
+  link: string
+  description: string
+  route: string
+}
+
+interface IGroups {
+  id: number,
+  group_name: string,
+  products_list: IProduct[]
+}
+
+function PageProductsClient ({ data }: { data: IGroups[] }) {
+  const [search, setSearch] = useState<string>('')
+  const [procutsData, setProductsData] = useState(data)
+  
+  const searchRule = useCallback((name: string) => {
+    const regex = new RegExp(search, 'i');
+    return regex.test(name);
+  }, [search])
+
+  console.log(data)
+  
+  useEffect(() => {
+    const searchList: IGroups[] = []
+    
+    data.forEach((groups) => {
+      const searchProduct = groups.products_list.filter((product) => searchRule(product.name))
+
+      if(searchProduct.length > 0) {
+        searchList.push({
+          id: groups.id,
+          group_name: groups.group_name,
+          products_list: searchProduct
+        })
+      }
+    })
+    
+    setProductsData(searchList)
+}, [search, searchRule, data]);
+
+  return (
+    <div className={styles.products_container}>
+      <div className={styles.products_search_bar}>
+        <Image
+          width={16}
+          height={16}
+          src={search_logo}
+          alt='icone de pesquisa'
+          className={styles.products_search_bar_icone}
+        />
+        <input 
+          placeholder='Pesquise por produtos'
+          onChange={(e) => setSearch(e.currentTarget.value)}
+        />
+      </div>
+
+      {procutsData?.map((item) => (
+        <div
+          key={item?.id}
+          className={styles.product_group}
+        >
+          <p className={styles.products_title}>
+            {item?.group_name}
+          </p>
+
+          <Swiper
+            slidesPerView={1.7}
+            spaceBetween={12}
+            className={styles.products_list}
+            breakpoints={{
+              320: {
+                slidesOffsetBefore: 12,
+                slidesPerView: 1.5,
+              },
+              375: {
+                slidesOffsetBefore: 12,
+                slidesPerView: 1.7,
+              },
+              425: {
+                slidesOffsetBefore: 12,
+                slidesPerView: 2,
+                spaceBetween: 22,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                slidesOffsetBefore: 20,
+                slidesOffsetAfter: 20
+              },
+              1024: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+                slidesOffsetBefore: 40,
+                slidesOffsetAfter: 40
+              },
+              1440: {
+                slidesPerView: 4.8,
+                spaceBetween: 20,
+                slidesOffsetBefore: 40,
+                slidesOffsetAfter: 40
+              },
+              2560: {
+                slidesPerView: 4.6,
+                spaceBetween: 30,
+                slidesOffsetBefore: 80,
+                slidesOffsetAfter: 80
+              }
+            }}
+          >
+            {item?.products_list?.map((product) => (
+              <SwiperSlide
+                key={product.id}
+                className={styles.product_item}
+              >
+                <img 
+                  src={product.image}
+                  alt={`imagem do ${product.name}`}
+                  className={styles.product_image}
+                />
+                <p className={styles.product_name}>{product.name}</p>
+                <p className={styles.product_description}>{product.description}</p>
+                <div
+                  className={styles.product_buttons_container}
+                >
+                  <div
+                    className={styles.product_button}
+                  >
+                    <Image
+                      src={whatsapp}
+                      sizes='(min-width: 1440px) 20px, 20px'
+                      alt='icone whatsapp'
+                      className={styles.product_item_icone}
+                    />
+                      <a
+                        className={styles.product_button_text}
+                        href={product.link}
+                        target='_blank'
+                      >
+                        Adquirir
+                      </a>
+                  </div>
+                  <div
+                    className={`${styles.product_button} ${styles.product_button_blue}`}
+                  >
+                      <a
+                        className={styles.product_button_text}
+                        href={`produtos/${product.route}`}
+                      >
+                        Sabiba mais
+                      </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default PageProductsClient
